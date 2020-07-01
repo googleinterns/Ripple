e4208be70bdec822c6269a0d9c4988fcac03da80
Sarah Wu <saraholiviawu@google.com>
2020-07-01 21:13:30 +0000

    Fix spacing for script.js.

--- ripple/src/main/webapp/script.js
+++ ripple/src/main/webapp/script.js
@@ -1,8 +1,8 @@
 // Display an alert containing the inputted address if user presses enter
 function searchAddress(e){
-	addressInput = document.getElementById("address-input").value;
-    if (e.keyCode === 13) {
-    	alert("You are searching: " + addressInput);
-    }
-	return false;
+  addressInput = document.getElementById("address-input").value;
+  if (e.keyCode === 13) {
+    alert("You are searching: " + addressInput);
+  }
+  return false;
 }
