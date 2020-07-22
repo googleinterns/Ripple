353771d33231716a1dc86d82b67a29cf4944293a
Smruthi Balajee <smruthibalajee@google.com>
2020-07-22 02:08:53 +0000

    fixes async issues with loading the geocoded city. adds a variable for enter key.

--- ripple/src/main/webapp/landing.js
+++ ripple/src/main/webapp/landing.js
@@ -1,43 +1,4 @@
-/* Address input and manipulation functions. */
-
-/* Function that takes the user inputted address and converts it to latitude and longitude coordinates. */
-function convertAddressToCoord(address) {
-  var geocoder = new google.maps.Geocoder();
-  geocoder.geocode( { 'address': address}, function(results, status) {
-    if (status == 'OK') {
-      //Stores user's latitude and longitude in localStorage, to be used for computing walking distance
-      localStorage.setItem('enteredLat', results[0].geometry.location.lat());
-      localStorage.setItem('enteredLong', results[0].geometry.location.lng());
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
-
-//TODO: use latitude and longitude to query businesses within a certain radius in future, when Firestore allows for Geopoint queries
-/* Given a latitude and longitude, finds the name of the city in the US with those coordinates. */
-function convertCoordToCity() {
-  var geocoder = new google.maps.Geocoder();
-  var latlng = { lat: parseFloat(localStorage.getItem('enteredLat')), lng: parseFloat(localStorage.getItem('enteredLong'))};
-  geocoder.geocode({ location: latlng }, function(results, status) {
-    if (status === "OK") {
-      if (results[0]) {
-        //Stores user's city in localStorage, to be used in the main page query
-        localStorage.setItem('enteredCity', getAddressComponent(results[0].address_components, 'locality'));
-      } else {
-        window.alert("No results found");
-      }
-    } else {
-      window.alert("Geocoder failed due to: " + status);
-    }
-  });
-}
-
-/* When a list of address components and the type is specified, returns the long name of the type requested. Uses map and filter to parse
-   the address_components arrays. */
-function getAddressComponent(components, type) {
-  return components.filter((component) => component.types.indexOf(type) === 0).map((item) => item.long_name).pop() || null;
-}
+/* Address input and autocomplete functions. */
 
 /* Function that utilizes the Place API to implement an autocomplete feature when a user is entering an address. */
 function searchAddressAutocomplete() {
@@ -66,19 +27,19 @@ function searchAddressAutocomplete() {
         ].join(' ');
         }
 
-        //Convert address if user clicks on the autocomplete suggestion
-        convertAddressToCoord(address);
-        convertCoordToCity();
+        //Set the input address to use in the main page
+        localStorage.setItem('inputAddress', address);
         window.location.assign("main.html");
     });
 }
 
 /* Display an alert containing the inputted address if user presses enter */
-function searchAddress(e) {
-  if (e.keyCode === 13) {
+function searchAddress(keyPress) {
+  var ENTER_KEY = 13;
+  if (keyPress.keyCode === ENTER_KEY) {
     var address = document.getElementById('address-input').value;
-    convertAddressToCoord(address);
-    convertCoordToCity();
+    //Set the input address to use in the main page
+    localStorage.setItem('inputAddress', address);
     window.location.assign("main.html");
   }
   return false;
--- ripple/src/main/webapp/main.js
+++ ripple/src/main/webapp/main.js
@@ -1,5 +1,53 @@
 /* Dynamic Carousel functions. */
 
+/* Function that takes the user inputted address and converts it to latitude and longitude coordinates. */
+function convertAddressToCoord() {
+  var address = localStorage.getItem('inputAddress');
+  var geocoder = new google.maps.Geocoder();
+  console.log("old" + localStorage.getItem('enteredLat') + localStorage.getItem('enteredLong'));
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == 'OK') {
+      //Stores user's latitude and longitude in localStorage, to be used for computing walking distance
+      localStorage.setItem('enteredLat', results[0].geometry.location.lat());
+      localStorage.setItem('enteredLong', results[0].geometry.location.lng());
+      console.log(localStorage.getItem('enteredLat'));
+      console.log(localStorage.getItem('enteredLong'));
+      convertCoordToCity();
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+//TODO: use latitude and longitude to query businesses within a certain radius in future, when Firestore allows for Geopoint queries
+/* Given a latitude and longitude, finds the name of the city in the US with those coordinates. */
+function convertCoordToCity() {
+  var geocoder = new google.maps.Geocoder();
+  console.log(localStorage.getItem('enteredLat')+ ' in coordToCity');
+  console.log(localStorage.getItem('enteredLong') + ' in coordToCity');
+  var latlng = { lat: parseFloat(localStorage.getItem('enteredLat')), lng: parseFloat(localStorage.getItem('enteredLong'))};
+  geocoder.geocode({ location: latlng }, function(results, status) {
+    if (status === "OK") {
+      if (results[0]) {
+        //Stores user's city in localStorage, to be used in the main page query
+        localStorage.setItem('enteredCity', getAddressComponent(results[0].address_components, 'locality'));
+        console.log('enteredCity ' + localStorage.getItem('enteredCity'));
+        loadCarousels();
+      } else {
+        window.alert("No results found");
+      }
+    } else {
+      window.alert("Geocoder failed due to: " + status);
+    }
+  });
+}
+
+/* When a list of address components and the type is specified, returns the long name of the type requested. Uses map and filter to parse
+   the address_components arrays. */
+function getAddressComponent(components, type) {
+  return components.filter((component) => component.types.indexOf(type) === 0).map((item) => item.long_name).pop() || null;
+}
+
 /* Carousel function that allows cards to move to the left or right by one. Establishes functionality of the carousel. */
   $("#card-carousel1").on("slide.bs.carousel", function(e) {
     var $e = $(e.relatedTarget);
@@ -41,6 +89,8 @@ function addDynamicCarousel(carouselId, tag) {
         querySnapshot.forEach((doc) => {
             //Check if the city in Firestore matches the city extracted from the user inputted address.
             //Also checks if the business's tag list contains the tag that the carousel requires.
+            alert(localStorage.getItem('enteredCity'));
+            alert(localStorage.getItem('inputAddress'));
             if (doc.data().city == localStorage.getItem('enteredCity') && doc.data().tags[tag] == true) {
               makeElement = true;
               const card = document.createElement('div');
