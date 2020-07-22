6dac359584a57666557c32d8cde754f5588ad3c7
Sarah Wu <saraholiviawu@google.com>
2020-07-22 10:38:41 +0000

    Resolve merge conflicts.

---
+++ ripple/src/main/webapp/posts.js
@@ -0,0 +1,116 @@
+/* Update function to fetch all comments for a particular post */
+function viewAllPostComments() {
+  alert("Fetch all comments for this post!");
+}
+
+/* Display an alert if user presses enter to comment on a post */
+function addComment(e) {
+  comment = document.getElementById("add-comment").value;
+  if (e.keyCode === 13) {
+    alert("You are commenting: " + comment);
+  }
+}
+
+// Add posts onto the Posts page TODO: ADD A LIMIT
+function addDynamicPosts() {
+  var postContainerElement = document.getElementById("post-container");
+  // For each document in this city, state, in reverse timestamp, create a post element & appendChild to postContainer
+  db.collection("posts")
+      .where("city", "==", "New Haven")
+      .where("state", "==", "CT")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          console.log("timestamp: " + doc.data().timestamp);
+          console.log("postOrder: " + doc.data().postOrder);
+          var uid = doc.data().uid;
+          console.log("uid: " + uid)
+          var caption = doc.data().caption;
+          var numberComments = doc.data().numberComments;
+          var blobKey = doc.data().blobKey;
+          // make sure that the postContainerElement finishes before the promises return
+          getNameFromFirestoreFuture('businesses', uid, function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+              // doc.data() is never undefined for query doc snapshots
+              businessName = doc.data().name;
+              console.log("name field for document within businesses: " + businessName);
+              getNameFromFirestoreFuture('users', uid, function(secondQuerySnapshot) {
+                secondQuerySnapshot.forEach(function(doc) {
+                  // doc.data() is never undefined for query doc snapshots
+                  userName = doc.data().name;
+                  console.log("name field for document within users: " + userName);
+                  console.log("userName: " + userName);
+                  console.log("businessName: " + businessName);
+                  postContainerElement.appendChild(createPostElement(userName, businessName, caption, numberComments, blobKey));
+                });
+              });
+            });
+
+          });
+       });
+  });
+}
+
+// Assumes only one doc exists for a query
+function getNameFromFirestoreFuture(collection, uid, lambda) {
+  console.log(collection + " " + uid);
+  var nameFuture = db.collection(collection)
+      .where("uid", "==", uid)
+      .get()
+      .then(lambda)
+      .catch(function(error) {
+          console.log("Error getting documents: ", error);
+      });
+}
+
+
+
+// Create a post element
+function createPostElement(userName, businessName, caption, numberComments, blobKey) {
+  const postElement = document.createElement('div');
+  postElement.classList = "post";
+  var content = `
+      <div class="post-subwrapper space-between">
+        <div class="post-header">
+          <img id="post-avatar" src="images/undrawAvatar.svg" class="post-profile-photo">
+          <div class="post-user-business">
+            <p class="boldest-text">${userName}</p>
+            <p>${businessName}</p>
+          </div>
+        </div>
+        <img class="post-share-icon" src="/images/post_share_icon.png">
+      </div>
+      <img src="/serve?blob-key=${blobKey}" class="post-photo image-resizing">
+      <div class="post-line-height"> 
+        <div class="post-subwrapper">
+          <div class="space-between">
+            <p>
+              <span class="post-margin-right">
+                <img class="heart-icon" src="/images/heart_icon.svg">
+              </span>
+               482
+            </p>
+            <p>2 hours ago</p>
+          </div>
+          <p>
+            <span class="boldest-text">${userName}</span>
+            ${caption}
+          </p>
+          <p class="post-view-all" onclick="viewAllPostComments()"> View all ${numberComments} comments</p>
+          <p>
+            <span class="boldest-text">Sarah Wu</span>
+            Love this photo!
+          </p>
+          <p>
+            <span class="boldest-text">Smruthi Balajee</span>
+            Wow, I'd really love to visit this place sometime!
+          </p>
+          <hr class="line-break-lighter">
+          <textarea class="post-comments" id="add-comment" placeholder="Commenting publicly..."
+              onkeypress="addComment(event)"></textarea>
+        </div>
+      </div>
+  `;
+  postElement.innerHTML = content;
+  return postElement;
+}
--- ripple/src/main/webapp/script.js
+++ ripple/src/main/webapp/script.js
@@ -1,36 +1,4 @@
-/* Carousel function that allows cards to move to the left or right by one. */
-$("#cardCarousel").on("slide.bs.carousel", function(e) {
-  var $e = $(e.relatedTarget);
-  var idx = $e.index();
-  var itemsPerSlide = 3;
-  var totalItems = $(".carousel-item").length;
-
-  if (idx >= totalItems - (itemsPerSlide - 1)) {
-    var it = itemsPerSlide - (totalItems - idx);
-    for (var i = 0; i < it; i++) {
-      // append slides to end
-      if (e.direction == "left") {
-        $(".carousel-item")
-          .eq(i)
-          .appendTo(".carousel-inner");
-      } else {
-        $(".carousel-item")
-          .eq(0)
-          .appendTo($(this).find(".carousel-inner"));
-      }
-    }
-  }
-});
-
-/* Display an alert containing the inputted address if user presses enter */
-function searchAddress(e) {
-  addressInput = document.getElementById("address-input").value;
-  if (e.keyCode === 13) {
-    alert("You are searching: " + addressInput);
-    window.location.assign("main.html");
-  }
-  return false;
-}
+/* Blobstore and post functions. */
 
 /* Given blob key and image id, inserts image from Blobstore */
 function serveBlob(blobKey, imageId) {
