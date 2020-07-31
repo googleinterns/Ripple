5d316add1edec9c866d2d557a9a2a53f8a783c6e
Sarah Wu <saraholiviawu@google.com>
2020-07-31 02:06:26 +0000

    Render map with displayed walking travel route.

---
+++ ripple/src/main/webapp/maps.js
@@ -0,0 +1,42 @@
+/* Maps JS API initialization */
+var mapKey = config.apiKey;
+var script = document.createElement("script");
+script.src = "https://maps.googleapis.com/maps/api/js?key=" + mapKey + "&callback=initMap&libraries=&v=weekly";
+script.defer = true;
+script.async = true;
+document.head.appendChild(script); 
+
+window.initMap = () => {
+  const directionsService = new google.maps.DirectionsService();
+  const directionsRenderer = new google.maps.DirectionsRenderer();
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 8,
+    // Import user's current location from local storage
+    center: { lat: 34.05, lng: -118.24 },
+    disableDefaultUI: true,
+    zoomControl: true,
+  });
+  directionsRenderer.setMap(map);
+  calculateAndDisplayRoute(directionsService, directionsRenderer);
+}
+
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+  directionsService.route(
+    {
+      origin: {
+        query: "Torrance, CA"
+      },
+      destination: {
+        query: "8386 Beverly Blvd"
+      },
+      travelMode: google.maps.TravelMode.WALKING
+    },
+    (response, status) => {
+      if (status === "OK") {
+        directionsRenderer.setDirections(response);
+      } else {
+        window.alert("Directions request failed due to " + status);
+      }
+    }
+  );
+}
