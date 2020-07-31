75e7bf61bc5a3c031eee15f40797622c6d71e150
Sarah Wu <saraholiviawu@google.com>
2020-07-31 19:51:29 +0000

    Fix merge conflicts.

--- ripple/src/main/webapp/businessgallery.js
+++ ripple/src/main/webapp/businessgallery.js
@@ -1,4 +1,4 @@
-/* Function to dynamically load the name of the gallery page. Will either be the user's search input or view all. */
+``````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````/* Function to dynamically load the name of the gallery page. Will either be the user's search input or view all. */
 function displayName() {
   console.log(localStorage.getItem("galleryPageName"));
   console.log(document.getElementById("left-align-header").value);
